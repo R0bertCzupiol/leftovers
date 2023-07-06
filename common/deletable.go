42f6c33ea8a3a3841b6482fe36921d370989e680
robertczupiol <robert.czupiol@mewe.com>
2023-07-06 15:14:27 +0200

    setup deletable list

--- common/deletable.go
+++ common/deletable.go
@@ -6,6 +6,6 @@ type Deletable interface {
 	Type() string
 }
 
-var CriticalFilter = [5]string{"prod", "live", "qa", "stage", "master"}
+var CriticalFilter = [6]string{"prod", "live", "qa", "ci.k8s.sgr-labs.com.", "k8s.sgr-cloud.sh.", "dev.k8s.sgr-cloud.sh."}
 
 var Debug bool = false
